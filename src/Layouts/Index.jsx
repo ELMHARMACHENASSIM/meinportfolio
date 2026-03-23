714b56b8a4f89cef782efc99fedc6c5234358f18
Nassim El M'harmache <n.elmharmache@gmail.com>
2026-03-23 18:34:55 +0100

    second commit 2

--- src/Layouts/Index.jsx
+++ src/Layouts/Index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./layout";
+import Layout from "./Layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Education from "../pages/Education";
